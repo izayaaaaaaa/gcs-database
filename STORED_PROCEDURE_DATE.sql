dc4bf98b55921d5aa6672310fe65e28170d0f0ee
Francyn Macadangdang <izaya.orihara0011@gmail.com>
2022-05-07 18:28:49 +0800

    modified stored procedure and project view

---
+++ STORED_PROCEDURE_DATE.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE PROCEDURE today_is AS
+BEGIN
+-- display the current system date in long format
+  DBMS_OUTPUT.PUT_LINE( 'Today is ' || TO_CHAR(SYSDATE, 'DL') );
+END today_is;
+
+BEGIN
+  today_is(); 
+END;
