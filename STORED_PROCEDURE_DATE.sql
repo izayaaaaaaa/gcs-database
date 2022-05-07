1022f569950a4be5d8350fb4c72b62de8c2ae0e5
Francyn Macadangdang <izaya.orihara0011@gmail.com>
2022-05-07 18:31:40 +0800

    updated date procedure

--- STORED_PROCEDURE_DATE.sql
+++ STORED_PROCEDURE_DATE.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE PROCEDURE today_is AS
+CREATE OR REPLACE PROCEDURE table_creation AS
 BEGIN
 -- display the current system date in long format
-  DBMS_OUTPUT.PUT_LINE( 'Today is ' || TO_CHAR(SYSDATE, 'DL') );
-END today_is;
+  DBMS_OUTPUT.PUT_LINE( 'Table creation ' || TO_CHAR(SYSDATE, 'DL') );
+END table_creation;
 
 BEGIN
-  today_is(); 
+  table_creation(); 
 END;
